06cee4d23eb3964e30c227b000ee67221b27ee46
tri/KUMA <trikuma@hotmail.com>
2025-09-28 23:40:30 +0800

    add very basic args parsing

--- src/program/args.rs
+++ src/program/args.rs
@@ -5,10 +5,10 @@ use color_eyre::eyre::Result;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ArgToken {
     // short or long flags with no value
-    Flag { name: String },
+    Flag(String),
 
     // short or long flags with an attached value
-    Param { name: String, value: String },
+    Param(String, String),
 
     // value with no associated flag
     Constant(String),
@@ -18,15 +18,42 @@ pub enum ArgToken {
 }
 
 pub fn tokenize_args(args: Args) -> Result<Vec<ArgToken>> {
-    let args = args.collect::<Vec<_>>()[1..]
-        .iter()
-        .map(|i| i.to_owned())
-        .collect::<Vec<_>>();
+    let mut args = args.into_iter().skip(1).peekable();
+    let mut tokens = Vec::new();
+    let mut terminator_processed = false;
 
-    let tokens = args
-        .iter()
-        .map(|i| ArgToken::Constant(i.clone()))
-        .collect::<Vec<_>>();
+    while let Some(arg) = args.next() {
+        if terminator_processed {
+            tokens.push(ArgToken::Constant(arg));
+        } else if arg == "--" {
+            tokens.push(ArgToken::Terminator);
+            terminator_processed = true;
+        } else if arg.starts_with('-') {
+            // TODO: it would be good to allow clustered short flags to be expanded (ie, '-abc' becomes '-a -b -c')
+
+            let trimmed_arg = arg.trim_start_matches('-');
+            let mut split_args = trimmed_arg.split('=').peekable();
+
+            let flag = split_args.next().unwrap().to_string();
+            let value: Option<String> = if split_args.peek().is_some() {
+                Some(split_args.collect())
+            } else if let Some(next_arg) = args.peek()
+                && !next_arg.starts_with('-')
+            {
+                Some(args.next().unwrap())
+            } else {
+                None
+            };
+
+            if let Some(value) = value {
+                tokens.push(ArgToken::Param(flag, value));
+            } else {
+                tokens.push(ArgToken::Flag(flag));
+            }
+        } else {
+            tokens.push(ArgToken::Constant(arg));
+        }
+    }
 
     Ok(tokens)
 }
